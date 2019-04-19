0af4c1e88c497d2b2fc34fcc35cd073b92382866
Kaephas <kkain@mail.greenriver.edu>
2019-04-19 16:55:41 -0400

    add post method to form, and routes to forms

--- index.php
+++ index.php
@@ -42,9 +42,24 @@ $f3->route('GET /@animal', function($f3, $params) {
             echo 'Rinadingding';
             break;
         default:
-            echo 'hello??';
+            $f3->error(404);
     }
 
+});
+
+$f3->route("GET /order", function() {
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
+$f3->route("POST /order2", function() {
+    $view = new Template();
+    echo $view->render('views/form2.html');
+});
+
+$f3->route("POST /results", function() {
+    $view = new Template();
+    echo $view->render('views/results.html');
 });
 
 //Run Fat-free
