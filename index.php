8072e79220801f82ff0cf447a22fd70c8ced5db8
Kaephas <kkain@mail.greenriver.edu>
2019-04-19 16:15:49 -0400

    create index and default route and stuff

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require vendor/autoload file
+require_once('vendor/autoload.php');
+
+//Create an instane of the Base class (instantiate Fat-Free)
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+//    $view = new Template();
+//    echo $view-> render('views/home.html');
+    echo "<h1>My Pets</h1>";
+    echo '<a href="order">Order a Pet</a>';
+});
+
+//Run Fat-free
+$f3->run();
